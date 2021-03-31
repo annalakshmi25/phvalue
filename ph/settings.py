3366339b8404e8fbae3f2355ed3992ca521dc8e7
annalakshmi25 <annalakshmiv25@gmail.com>
2021-03-31 16:14:17 +0530

    edited

--- ph/settings.py
+++ ph/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0=*#j4ar4u6-#bdna1^1suyrg_4fx*7&urjy5^(x93#62a5eud'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.222.141.214','localhost']
+ALLOWED_HOSTS = ['18.191.178.125','localhost']
 
 
 # Application definition
